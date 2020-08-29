1613aab565c0c1d48616c79aacac1a6de1cd9b3c
Jeffrey Bergier <jeffburg@jeffburg.com>
2020-08-29 11:23:01 +0900

    updated LAMBDA file with clearer errors while debugging

--- LambdaFunction.js
+++ LambdaFunction.js
@@ -1,29 +1,34 @@
 const crypto = require('crypto');
 const aws = require('aws-sdk');
 const ses = new aws.SES({region: 'us-east-2'});
+const DEBUG = false;
 const kSecretKey = process.env.SECRET;
 const kEmailFrom = process.env.EMAIL_FROM;
 const kEmailTo   = process.env.EMAIL_TO;
 
 exports.handler = async (event, context, callback) => {
   // check environment variable
-  if (!(typeof kSecretKey === 'string' 
+  if (!(typeof kSecretKey === 'string'
      && typeof kEmailFrom === 'string'
      && typeof kEmailTo   === 'string'))
   {
-    return { statusCode: 403, body:'' };
+    if (DEBUG) { return { statusCode: 410, body:'KEYS MISSING' };
+    } else { return { statusCode: 403, body:'' }; }
   }
   // check make sure request is right
   if (event.requestContext.http.method != "PUT") {
-    return { statusCode: 403, body:'' };
+    if (DEBUG) { return { statusCode: 409, body:'WRONG HTTP TYPE' };
+    } else { return { statusCode: 403, body:'' }; }
   }
   if (event.requestContext.http.path != "/log.cgi") {
-    return { statusCode: 403, body:'' };
+    if (DEBUG) { return { statusCode: 408, body:'WRONG ENDPOINT' };
+    } else { return { statusCode: 403, body:'' }; }
   }
   if (!('mac' in event.queryStringParameters)) {
-    return { statusCode: 403, body:'' };
+    if (DEBUG) { return { statusCode: 407, body:'MISSING MAC SIGNATURE' };
+    } else { return { statusCode: 403, body:'' }; }
   }
-  
+
   try {
     // do HMAC signature verification
     const secretKey = Buffer.from(kSecretKey, 'base64');
@@ -35,37 +40,32 @@ exports.handler = async (event, context, callback) => {
                             .digest();
     const rhsSignatureB64 = Buffer.from(rhsSignature, 'binary').toString('base64');
     if (lhsSignatureB64 != rhsSignatureB64) {
-      return { statusCode: 403, body:'' };
+        if (DEBUG) { return { statusCode: 406, body:'SIGNATURE MISMATCH' };
+        } else { return { statusCode: 403, body:'' }; }
     }
-    
-    // compose email
-    const requestEvent = JSON.parse(requestBody);
-    const functionName = requestEvent["logDetails"]["functionName"];
-    const filePath = requestEvent["logDetails"]["fileName"];
-    var fileName;
     try {
-      // attempt to get just the last path component out of this string
-      fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
-    } catch(error) {
-      fileName = filePath;
-    }
-    const emailSubject = '[' + requestEvent["incident"] + '] ' + fileName + ' - ' + functionName;
-    const emailBody = JSON.stringify(requestEvent, null, '    '); // Pretty print with 4 spaces
-    
-    // send email
-    const params = { Destination: { ToAddresses: [kEmailTo] },
-                     Message: { Body: { Text: { Data: emailBody } },
-                                Subject: { Data: emailSubject } },
-                     Source: kEmailFrom };
+        // compose email
+        const requestEvent = JSON.parse(requestBody);
+        const functionName = requestEvent["logDetails"]["functionName"];
+        const fileName = requestEvent["logDetails"]["fileName"];
+        const emailSubject = '[' + requestEvent["incident"] + '] ' + fileName + ' - ' + functionName;
+        const emailBody = JSON.stringify(requestEvent, null, '    '); // Pretty print with 4 spaces
 
-    const request = ses.sendEmail(params);
-    await request.promise()
-    return { statusCode: 200, body:'SUCCESS' };
+        // send email
+        const params = { Destination: { ToAddresses: [kEmailTo] },
+                         Message: { Body: { Text: { Data: emailBody } },
+                                    Subject: { Data: emailSubject } },
+                         Source: kEmailFrom };
+
+        const request = ses.sendEmail(params);
+        await request.promise()
+        return { statusCode: 200, body:'SUCCESS' };
+    } catch(error) {
+        if (DEBUG) { return { statusCode: 404, body:'ERROR SENDING EMAIL' };
+        } else { return { statusCode: 403, body:'' }; }
+    }
   } catch(error) {
-    return { statusCode: 403, body:'' };
-    // debugging
-    // return { statusCode: 400, body:error.message };
+      if (DEBUG) { return { statusCode: 405, body:'ERROR GENERATING SIGNATURE' };
+      } else { return { statusCode: 403, body:'' }; }
   }
-  // Debugging
-  // return { statusCode: 400, body:JSON.stringify(event) };
 };
