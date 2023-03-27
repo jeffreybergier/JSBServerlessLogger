e8d66a1e72975095e2d90cbb1f75d2a361108e96
jeffreybergier <jeffburg@jeffburg.com>
2023-03-27 15:56:29 +0900

    updated the lambda function to support newer nodeJS lambdas

--- LambdaFunction.js
+++ LambdaFunction.js
@@ -1,6 +1,33 @@
+//
+//  Created by Jeffrey Bergier on 2020/08/24.
+//
+//  MIT License
+//
+//  Copyright (c) 2020 Jeffrey Bergier
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+//
+
 const crypto = require('crypto');
-const aws = require('aws-sdk');
-const ses = new aws.SES({region: 'us-east-2'});
+const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
+const ses = new SESClient({ region: "us-east-2" });
 const DEBUG = process.env.DEBUG_MODE === 'DEBUG';
 const kSecretKey = process.env.SECRET;
 const kEmailFrom = process.env.EMAIL_FROM;
@@ -56,9 +83,8 @@ exports.handler = async (event, context, callback) => {
                          Message: { Body: { Text: { Data: emailBody } },
                                     Subject: { Data: emailSubject } },
                          Source: kEmailFrom };
-
-        const request = ses.sendEmail(params);
-        await request.promise()
+        const command = new SendEmailCommand(params);
+        await ses.send(command);
         return { statusCode: 200, body:'SUCCESS' };
     } catch(error) {
         if (DEBUG) { return { statusCode: 404, body:'ERROR SENDING EMAIL' };
